232a14033a59fe1d2f54175818d19f3f140ca723
Abhinav Gupta <105072614+abhi-navz@users.noreply.github.com>
2023-03-10 17:05:14 +0530

    Higher order functions , timeinterval timeout

---
+++ Javascript/march10 setinterval and timeout.js
@@ -0,0 +1,9 @@
+// setInterval
+
+const hello = () =>{
+    console.log("Hello Abhinav!", Math.random());
+}
+// setInterval(hello, 1000) // 1000 here is 1000ms that is 1 sec
+
+//  setTimeout
+setTimeout(hello, 2000);
